d12e18d803f6ecad79d45e26e0631273a5e396fe
Daksh Mehta <daksh@dristal.com>
2020-03-28 19:34:22 +0530

    Added error handling for noCache middleware

--- Http/Middleware/NoCacheMiddleware.php
+++ Http/Middleware/NoCacheMiddleware.php
@@ -6,10 +6,16 @@ class NoCacheMiddleware {
     public function handle($request, \Closure $next)
     {
         $response = $next($request);
-
         
-        return $response->header('Cache-Control','nocache, no-store, max-age=0, must-revalidate')
-            ->header('Pragma','no-cache')
-            ->header('Expires','Fri, 01 Jan 1990 00:00:00 GMT');
+        try {
+            return $response->header('Cache-Control','nocache, no-store, max-age=0, must-revalidate')
+                ->header('Pragma','no-cache')
+                ->header('Expires','Fri, 01 Jan 1990 00:00:00 GMT');
+        }
+        catch(\Error $e){
+            // Happened when exporting the table.
+
+            return $response;
+        }
     }
 }
